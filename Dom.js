1b03b5a4e2bed19ae23eb54882ff71067b88e36b
sahalshaikhh <104682780+sahalshaikhh@users.noreply.github.com>
2022-08-16 22:38:39 +0530

    Add files via upload

---
+++ Dom.js
@@ -0,0 +1,19 @@
+function Externalalert(){
+    alert("Hello User");
+}
+function Externalconfirm(){
+    if(confirm("Are you sure..??"))
+        {
+            alert("YES");
+        }
+        else
+        {
+            alert("NO");
+        }
+}
+function Externalprompt(){
+        var FName=prompt("Enter First Name Here..");
+        var LName=prompt("Enter Last Name Here..");
+        alert(FName +" "+ LName);
+
+}
